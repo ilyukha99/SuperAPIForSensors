96b0f9fda4bc16233f01fb559b57c3f2b7721473
hater <ndrozdov123@gmail.com>
2021-05-07 13:13:00 +0700

    refactored package

--- src/main/java/org/SuperApiForSensors/dao/DAO.java
+++
@@ -1,8 +0,0 @@
-package org.SuperApiForSensors.dao;
-
-public interface DAO<Entity, Key> {
-    void create(Entity entity);
-    Entity read(Key key);
-    void update(Entity entity);
-    void delete(Entity entity);
-}
--- src/main/java/org/SuperApiForSensors/model/Room.java
+++
@@ -1,5 +0,0 @@
-package org.SuperApiForSensors.model;
-
-public class Room {
-
-}
